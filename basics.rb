39991e972ce9996ba85c1ac967ea5986b1262a47
Israt Ahmed <110242882+IsratAhmed@users.noreply.github.com>
2023-05-22 14:49:44 +0100

    Update basics.rb

--- basics.rb
+++ basics.rb
@@ -1,11 +1,11 @@
-#first comment
+# first comment
 
 =begin
 another comment
 =end
 
 print "What's your first name? "
-first_name = gets.chomp
+first_name = gets.chomp # gets user input to be outputted
 first_name.capitalize!
 
 print "What's your last name? "
@@ -18,6 +18,6 @@ city.capitalize!
 
 print "What state or province are you from? "
 state = gets.chomp
-state.upcase!
+state.upcase! # changes output into UPPERCASE
 
-puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!" # prints information that has been provided
