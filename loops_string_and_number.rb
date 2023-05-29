1fd75c337ae57264f84a4e1c215f645236900cba
Israt Ahmed <110242882+IsratAhmed@users.noreply.github.com>
2023-05-29 15:29:33 +0100

    Update loops_string_and_number.rb

--- loops_string_and_number.rb
+++ loops_string_and_number.rb
@@ -1,6 +1,6 @@
 counter = 0
 loop do
-  counter += 1
-  print "Ruby!"
+  counter += 1 # incrementing the counter
+  print "Ruby!" # outputting "Ruby!" by incrementing, but not outputting the counter
   break if counter == 30
 end
